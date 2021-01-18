481cab749b7358db5fef06be22f0ca3113d15565
Dustin Lacewell <dlacewell@gmail.com>
2021-01-18 12:07:59 -0600

    Fix command decorator

--- src/decorators/command.ts
+++ src/decorators/command.ts
@@ -5,10 +5,10 @@ export const commandRegistry = {};
 
 export default function command(name: string) {
     function D<T extends { new(...args: any[]): {} }>(constructor: T) {
-        commandRegistry[name] = constructor;
         const newClass = class extends constructor {
             name = name;
         }
+        commandRegistry[name] = newClass;
         return injectable()(newClass);
     }
     return D;
