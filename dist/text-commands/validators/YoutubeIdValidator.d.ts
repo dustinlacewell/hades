ad778f251c23546a8b13246c714ecf7368bc62a6
Dustin Lacewell <dustin@volleythat.com>
2022-06-14 01:53:05 -0500

    Remove YoutubeIdValidator

--- dist/text-commands/validators/YoutubeIdValidator.d.ts
+++
@@ -1,7 +0,0 @@
-import { TextArgumentInstaller } from '../factories/TextArgumentInstaller';
-import { TextCommandContext } from '../models/TextCommandContext';
-import { Validator } from './Validator';
-export declare class YoutubeIdValidator extends Validator {
-    static _channelIdRegExp: RegExp;
-    validate(arg: TextArgumentInstaller, ctx: TextCommandContext, value: any): Promise<any>;
-}
