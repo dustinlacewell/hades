3f033aadf298b294de7d1a2f35fcf7c76a179889
gnuhc <jonnnc@gmail.com>
2022-06-26 18:28:30 -0700

    Add test logic

--- src/slash-commands/services/SlashCommandBotService.ts
+++ src/slash-commands/services/SlashCommandBotService.ts
@@ -1,4 +1,4 @@
-import { BaseCommandInteraction } from 'discord.js';
+import { BaseCommandInteraction, Message } from 'discord.js';
 import { inject, injectable } from "inversify";
 import { HadesBotService } from "../../services/HadesBotService";
 import { SlashCommandService } from "./SlashCommandService/SlashCommandService";
@@ -13,5 +13,10 @@ export class SlashCommandBotService extends HadesBotService {
 
     async onInteractionCreated<T extends BaseCommandInteraction>(interaction: T) {
         this.commandService.dispatch(interaction);
+    }
+
+    async onMessage<T extends Message>(msg: T) {
+      msg.reply("test")
+      // this.commandService.dispatch(interaction);
     }
 }
